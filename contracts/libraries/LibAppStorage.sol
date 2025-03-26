97f328fe9f90782856c7ab26199d7798bbd2559b
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2025-03-26 23:22:16 +0100

    facets in place; getting ready for test

--- contracts/libraries/LibAppStorage.sol
+++ contracts/libraries/LibAppStorage.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 // import "../interfaces/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 struct Staker {
     uint256 erc20Staked;
